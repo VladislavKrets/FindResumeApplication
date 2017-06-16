9b39d6b524f4f46d132287968811b4c2a694267f
Vladislav <vlad-lollipop@ro.ru>
2017-06-17 00:06:03 +0300

    bug fix

--- src/main/java/com/findresume/app/Main.java
+++ src/main/java/com/findresume/app/Main.java
@@ -1,10 +1,13 @@
 package com.findresume.app;
 
+import com.findresume.app.model.Model;
+import com.findresume.app.model.dao.ResumeDaoImpl;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class Main extends Application {
 
@@ -21,6 +24,13 @@ public class Main extends Application {
         controller.setMain(this);
     }
 
+    @Override
+    public void stop() throws Exception {
+        ResumeDaoImpl.getSessionFactory().close();
+        StandardServiceRegistryBuilder.destroy(ResumeDaoImpl.getSessionFactory()
+                .getSessionFactoryOptions().getServiceRegistry());
+        super.stop();
+    }
 
     public static void main(String[] args) {
         launch(args);
--- src/main/java/com/findresume/app/model/Model.java
+++ src/main/java/com/findresume/app/model/Model.java
@@ -39,4 +39,5 @@ public class Model {
     public ResumeDaoImpl getResumeDao() {
         return resumeDao;
     }
+
 }
